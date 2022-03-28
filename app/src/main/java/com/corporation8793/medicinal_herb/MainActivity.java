7cef5075c925b3fe5748b194b63941142c5310db
gyeong <gyeongyeongi22@gmail.com>
2022-03-28 16:14:19 +0900

    깃 연결 확인

--- app/src/main/java/com/corporation8793/medicinal_herb/MainActivity.java
+++ app/src/main/java/com/corporation8793/medicinal_herb/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    // android studion version update (Bumblebee)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

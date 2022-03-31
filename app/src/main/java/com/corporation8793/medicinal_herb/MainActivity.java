91528b8eb04d630af134604effb569872e617b3e
Doo-Dong <ehd6754@gmail.com>
2022-03-31 16:35:20 +0900

    [TEST] 산야초 WP 웹 앱

--- app/src/main/java/com/corporation8793/medicinal_herb/MainActivity.java
+++ app/src/main/java/com/corporation8793/medicinal_herb/MainActivity.java
@@ -1,14 +1,45 @@
 package com.corporation8793.medicinal_herb;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 public class MainActivity extends AppCompatActivity {
-    // android studion version update (Bumblebee)
+    WebView wv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wv = findViewById(R.id.wv);
+
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.getSettings().getLoadsImagesAutomatically();
+        wv.getSettings().getUseWideViewPort();
+
+        wv.loadUrl("3.37.133.132");
+
+
+        wv.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (wv.canGoBack()) {
+            wv.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
